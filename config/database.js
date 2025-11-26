7f97091dd6c51944fc55071376a8a2efc1e885ab
QuadroObras <sistema@quadroobras.com>
2025-11-26 14:01:34 -0300

    fix: Aceitar MYSQL_DATABASE com underline

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
     user: process.env.MYSQLUSER || process.env.DB_USER || 'root',
     password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '',
-    database: process.env.MYSQLDATABASE || process.env.DB_NAME || 'controle_bobinas',
+    database: process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE || process.env.DB_NAME || 'controle_bobinas',
     port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
