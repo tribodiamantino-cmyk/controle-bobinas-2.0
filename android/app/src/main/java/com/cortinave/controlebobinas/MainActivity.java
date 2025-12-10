fbcf604365e9faffa0b5f7695b5713afa340f64a
QuadroObras <sistema@quadroobras.com>
2025-12-10 09:47:46 -0300

    feat: v2.2.2 - mudanca de scheme https->http para forcar invalidacao de cache

--- android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
+++ android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends BridgeActivity {
             WebSettings settings = getBridge().getWebView().getSettings();
             settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
             
-            android.util.Log.d("MainActivity", "🔥 Cache WebView DESABILITADO! v2.2.1-20251210-0915");
+            android.util.Log.d("MainActivity", "🔥 Cache WebView DESABILITADO! v2.2.2-20251210-0935");
         }
     }
 }
