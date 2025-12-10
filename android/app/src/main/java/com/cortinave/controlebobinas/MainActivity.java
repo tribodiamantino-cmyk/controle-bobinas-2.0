752f7c63e7584e2a9dba70468758969d6118ec06
QuadroObras <sistema@quadroobras.com>
2025-12-10 09:28:37 -0300

    feat: v2.2.1 - LOAD_NO_CACHE + versao visivel + timestamps atualizados

--- android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
+++ android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
@@ -2,6 +2,7 @@ package com.cortinave.controlebobinas;
 
 import android.os.Bundle;
 import android.webkit.CookieManager;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,12 +12,18 @@ public class MainActivity extends BridgeActivity {
         
         // FORÇAR LIMPEZA DE CACHE - Debug WebView cache teimoso
         if (getBridge() != null && getBridge().getWebView() != null) {
+            // Limpar cache e histórico
             getBridge().getWebView().clearCache(true);
             getBridge().getWebView().clearHistory();
             CookieManager.getInstance().removeAllCookies(null);
             CookieManager.getInstance().flush();
             
-            android.util.Log.d("MainActivity", "🔥 Cache WebView FORÇADAMENTE LIMPO!");
+            // DESABILITAR CACHE COMPLETAMENTE
+            WebSettings settings = getBridge().getWebView().getSettings();
+            settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+            settings.setAppCacheEnabled(false);
+            
+            android.util.Log.d("MainActivity", "🔥 Cache WebView DESABILITADO! v2.2.1-20251210-0915");
         }
     }
 }
