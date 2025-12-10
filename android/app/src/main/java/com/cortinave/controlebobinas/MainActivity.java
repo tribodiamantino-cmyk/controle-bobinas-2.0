0e88e273c70864ec79774c8de6ac527d2a1027cd
QuadroObras <sistema@quadroobras.com>
2025-12-10 08:50:38 -0300

    feat: NUCLEAR - clearCache() no MainActivity para matar cache WebView teimoso

--- android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
+++ android/app/src/main/java/com/cortinave/controlebobinas/MainActivity.java
@@ -1,5 +1,22 @@
 package com.cortinave.controlebobinas;
 
+import android.os.Bundle;
+import android.webkit.CookieManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // FORÇAR LIMPEZA DE CACHE - Debug WebView cache teimoso
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().clearCache(true);
+            getBridge().getWebView().clearHistory();
+            CookieManager.getInstance().removeAllCookies(null);
+            CookieManager.getInstance().flush();
+            
+            android.util.Log.d("MainActivity", "🔥 Cache WebView FORÇADAMENTE LIMPO!");
+        }
+    }
+}
